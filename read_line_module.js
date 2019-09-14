81ba068fcc50222a108474aa8245e19450de17cb
Digender <mdigender@gmail.com>
2019-09-14 13:34:45 +0530

    read line module tutorials

---
+++ read_line_module.js
@@ -0,0 +1,28 @@
+const readline = require('readline');
+const rl = readline.createInterface({input:process.stdin, output: process.stdout});
+let num1 = Math.floor((Math.random() * 10) + 1);
+let num2 = Math.floor((Math.random() * 10) + 1);
+let answer = num1 + num2;
+rl.question(`What is tha addition of ${num1} and ${num2}? \n`, (userInput) => {
+    if ((answer) == userInput.trim()) {
+        console.log('Correct anser')
+        rl.close() //this emits close event
+    } else {
+        rl.setPrompt('Incorrect nswer please try again \n');
+        rl.prompt();
+        rl.on('line', (userInput) => {
+            if (userInput.trim() == answer) {
+                rl.close();
+            } else {
+                rl.setPrompt(`Your answer of ${userInput} is incorrect try again\n`);
+                rl.prompt();
+            }
+        })
+    }
+});
+
+rl.on('close', () => {
+    console.log('Correct answer');
+})
+
+console.log()
