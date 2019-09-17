1c14879516d73ccc1753291d0858f0bccc25650d
Digender <mdigender@gmail.com>
2019-09-17 22:58:40 +0530

    get requests in Express and serving static files

--- express/app.js
+++ express/app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { //home page or index route
 	res.send('Hello World Express');
+});
+
+app.get('/example', (req, res) => { //home page or index route
+	res.send('Hitting example route');
+});
+
+app.get('/example/:name/:age', (req, res) => {
+	console.log(req.params);
+	console.log(req.query);
+	res.send('name: ' + req.params.name + ' age: ' + req.params.age);
+});
+
+app.use('', express.static(path.join(__dirname, '/static')))
+app.get('/index', (req, res) => { //home page or index route
+	res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 
 app.listen(3000);
